8c1834e0347195d8a0bcaab6adec13292a374185
polarrrr123 <148021744+polarrrr123@users.noreply.github.com>
2023-11-25 22:26:43 +0800

    Add files via upload

---
+++ se(A3)/todoModel.php
@@ -0,0 +1,47 @@
+<?php
+require('dbconfig.php');
+
+function getCartList() {
+	global $db;
+	$sql = "select * from cart;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+
+function addJob($jobName,$jobUrgent,$jobContent) {
+	global $db;
+
+	$sql = "insert into todo (jobName, jobUrgent, jobContent) values (?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "sss", $jobName, $jobUrgent,$jobContent); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+
+function delCartItem($id) {
+	global $db;
+
+	$sql = "delete from cart where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+
+function updateCartAmount($id, $amount) {
+	global $db;
+
+	$sql = "update cart set myAmount=? where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "ii", $amount, $id); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+?>
