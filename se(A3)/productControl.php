0aaf18c7cf7aa0d82c729dd296924f006e75b643
andrewlo1127 <148021525+andrewlo1127@users.noreply.github.com>
2023-11-26 21:52:16 +0800

    Update productControl.php

--- se(A3)/productControl.php
+++ se(A3)/productControl.php
@@ -1,7 +1,7 @@
 <?php
 require('productModel.php');
 
-$act = $_REQUEST['act'];
+$act = isset($_REQUEST['act']) ? $_REQUEST['act'] : '';
 
 switch ($act) {
     case "listProduct":
@@ -9,11 +9,20 @@ switch ($act) {
         echo json_encode($products);
         break;
     case "addToCart":
-        $jsonStr = $_POST['dat'];
+        $jsonStr = file_get_contents("php://input"); // 使用此行來獲取 JSON 數據
         $product = json_decode($jsonStr);
+        var_dump($jsonStr);
+        var_dump($product);
+
         // 在這裡應該加入一些驗證，確保傳入的數據是有效的
-        if ($product && isset($product->productName) && isset($product->productCost) && isset($product->productContent)) {
-            addToCart($product->productName, $product->productCost, $product->productContent, $product->id);
+        if ($product && isset($product->dat->productName) && isset($product->dat->productCost) && isset($product->dat->productId)) {
+            $result = addToCart($product->dat->productName, $product->dat->productCost, $product->dat->productId);
+
+            if ($result) {
+                echo json_encode(["success" => true]);
+            } else {
+                echo json_encode(["error" => "Failed to add to cart"]);
+            }
         } else {
             // 如果數據無效，可以返回一個錯誤狀態碼或者其他錯誤信息
             echo json_encode(["error" => "Invalid data"]);
@@ -21,6 +30,6 @@ switch ($act) {
         break;
     default:
         // 如果 act 不匹配任何預期的值，可以返回一個錯誤狀態碼或者其他錯誤信息
-        echo json_encode(["error" => "Invalid action"]);
+        echo json_encode(["error" => "Invalid action: $act"]);
 }
 ?>
