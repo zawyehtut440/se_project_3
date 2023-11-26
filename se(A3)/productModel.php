bfb50a417d7fd7beb38809ac1b945cf2dfd4ae82
andrewlo1127 <148021525+andrewlo1127@users.noreply.github.com>
2023-11-26 21:52:01 +0800

    Update productModel.php

--- se(A3)/productModel.php
+++ se(A3)/productModel.php
@@ -16,16 +16,44 @@ function getProductList() {
 }
 
 function addToCart($productName, $productCost, $productId) {
-	global $db;
-	// 假設購物車內容存儲在另一個資料表，這裡只是一個簡單的示例
-	// 在實際情況中，您可能需要更複雜的邏輯和資料庫結構
-	// 在這裡您可以將商品添加到購物車，可能需要檢查商品是否已經在購物車中，如果是，則增加數量
+    global $db;
 
-	// 以下是一個簡單的示例，假設購物車內容存儲在 cart 資料表中
-	$sql = "INSERT INTO cart (productName, productPrice, id) VALUES (?, ?, ?)";
-	$stmt = mysqli_prepare($db, $sql);
-	mysqli_stmt_bind_param($stmt, "sii", $productName, $productCost, $productId);
-	mysqli_stmt_execute($stmt);
-	return true;
+    // 先檢查購物車中是否已經存在相同的商品
+    $checkSql = "SELECT id, myAmount FROM cart WHERE productName = ? AND productPrice = ? AND id = ?";
+    $checkStmt = mysqli_prepare($db, $checkSql);
+    mysqli_stmt_bind_param($checkStmt, "sii", $productName, $productCost, $productId);
+    mysqli_stmt_execute($checkStmt);
+    $checkResult = mysqli_stmt_get_result($checkStmt);
+
+    if ($checkResult && $existingProduct = mysqli_fetch_assoc($checkResult)) {
+        // 商品已存在於購物車中，更新數量
+        $existingProductId = $existingProduct['id'];
+        $newAmount = $existingProduct['myAmount'] + 1;
+
+        $updateSql = "UPDATE cart SET myAmount = ? WHERE id = ?";
+        $updateStmt = mysqli_prepare($db, $updateSql);
+        mysqli_stmt_bind_param($updateStmt, "ii", $newAmount, $existingProductId);
+        mysqli_stmt_execute($updateStmt);
+
+        if (mysqli_stmt_affected_rows($updateStmt) > 0) {
+            return true; // 更新成功
+        } else {
+            return false; // 更新失敗
+        }
+    } else {
+        // 商品不存在於購物車中，插入新商品
+        $insertSql = "INSERT INTO cart (productName, productPrice, id, myAmount) VALUES (?, ?, ?, 1)";
+        $insertStmt = mysqli_prepare($db, $insertSql);
+        mysqli_stmt_bind_param($insertStmt, "sii", $productName, $productCost, $productId);
+        mysqli_stmt_execute($insertStmt);
+
+        if (mysqli_stmt_affected_rows($insertStmt) > 0) {
+            return true; // 插入成功
+        } else {
+            return false; // 插入失敗
+        }
+    }
 }
+
+
 ?>
