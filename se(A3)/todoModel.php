4a20a29f59d3f8b01e5eb64fe919b2307ec5a2a9
MSI\leoho <leoho202@gmail.com>
2023-11-27 01:20:37 +0800

    delete unnecessary code in todoModel.php

--- se(A3)/todoModel.php
+++ se(A3)/todoModel.php
@@ -15,16 +15,6 @@ function getCartList() {
 	return $rows;
 }
 
-function addJob($jobName,$jobUrgent,$jobContent) {
-	global $db;
-
-	$sql = "insert into todo (jobName, jobUrgent, jobContent) values (?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "sss", $jobName, $jobUrgent,$jobContent); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
-	return True;
-}
-
 function delCartItem($id) {
 	global $db;
 
